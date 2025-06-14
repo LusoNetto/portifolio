853db947f69fcad75da509aef8a61983a02cfbfc
Luso Netto <lusonetto137@outlook.com>
2025-06-14 00:25:56 -0300

    remove: remove a text description

--- src/Pages/Home/sections/AboutSection/AboutSection.tsx
+++ src/Pages/Home/sections/AboutSection/AboutSection.tsx
@@ -60,22 +60,6 @@ const AboutSection: React.FC = () => {
 
                     </Grid>
                 </Grid>
-                <Box pb={1}>
-                    <Typography>
-                        Como desenvolvedor fullstack, atuo na criação e manutenção de sistemas em diferentes tecnologias, focando nas boas práticas de desenvolvimento e nos fundamentos de engenharia de software.
-<br/>• Desenvolvimento e manutenção <strong>full-stack</strong> de aplicações com stacks <strong>TypeScript/Node</strong> <strong>(Express), PHP/Laravel e Java/Spring</strong>
-<br/>• Prática na criação de aplicações utilizando padrões <strong>REST</strong> e <strong>MVC</strong>
-<br/>• Utilização de padrões arquiteturais e design patterns
-<br/>• Conhecimento em banco de dados <strong>SQL</strong> <strong>(PostgreSQL, MySQL)</strong> e <strong>NoSQL (Redis, DynamoDB, MongoDB)</strong>
-<br/>• Pipelines <strong>CI/CD</strong> com <strong>GitHub Actions</strong> para reduzir tempo de entrega
-<br/>• Uso de <strong>Docker</strong> para containerização de aplicações, garantindo portabilidade e escalabilidade.
-<br/>• Escrita de testes automatizados (unitários, integração) para <strong>back-end</strong> e <strong>front-end</strong>, utilizando <strong>Vitest</strong>, e testes <strong>E2E</strong> com <strong>Cypress</strong> e <strong>Selenium</strong>
-<br/>• Participação ativa em decisões arquiteturais, garantindo um software escalável, modular e de fácil manutenção
-<br/>• <strong>Code reviews</strong> e boas práticas de desenvolvimento, garantindo qualidade de código e aderência a padrões como <strong>SOLID</strong> e <strong>Clean Code</strong>
-<br/>• Experiência em <strong>metodologias ágeis</strong> com <strong>Scrum</strong> e <strong>Kanban</strong>
-<br/>• Conhecimento em fundamentos de gerenciamento de filas com <strong>BullMQ</strong>
-                    </Typography>
-                </Box>
                 <hr />
                 <Box id="skills" pt={1} mb={3}>
                     <Typography variant="h3" textAlign="center" fontWeight={300}>Habilidades</Typography>
